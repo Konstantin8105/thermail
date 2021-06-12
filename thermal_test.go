8337643c0cf9f7889f9fe869159dc3edb06b45ee
Konstantin8105 <Konstantin8105@gmail.com>
2021-06-13 00:02:01 +0300

    example 1a is ok

--- thermal_test.go
+++ thermal_test.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func Example() {
+func ExampleProblem1() {
 	Q, T, err := Flat(os.Stdout, 450.0, []Layer{
 		Layer{
 			Thk: 4.0,
@@ -33,4 +33,33 @@ func Example() {
 	//  LAYER    INSULATION    CONDUCTIVITY   RESISTANCE TEMPERATURE,F
 	//     No  THICKNESS,in  BTU.IN/HR.SF.F  HR.SF.F/BTU  INSIDE OUTSIDE
 	//      0          4.00           0.337        11.87  450.00 16.09
+}
+
+func ExampleProblem1a() {
+	Q, T, err := Flat(os.Stdout, 450.0, []Layer{
+		Layer{
+			Thk: 4.5,
+			Conductivity: func(F float64) float64 {
+				return math.Exp(-1.62 + 0.213e-2*F)
+			},
+		},
+	}, 10.0, 6.0)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "%v", err)
+		return
+	}
+	_ = Q
+	_ = T
+
+	// Output:
+	// HEAT FLOW AND SURFACE TEMPERATURES OF INSULATED EQUPMENT PER ASTM C-680
+	//
+	//  EQUPMENT SERVICE TEMPERATURE, F: 450.0
+	//           AMBIENT TEMPERATURE, F: 10.0
+	//  SURFACE COEF. USED, BTU/HR.SF.F: 6.0
+	//       TOTAL HEAT FLUX, BTU/HR.SF: 32.53
+	//
+	//  LAYER    INSULATION    CONDUCTIVITY   RESISTANCE TEMPERATURE,F
+	//     No  THICKNESS,in  BTU.IN/HR.SF.F  HR.SF.F/BTU  INSIDE OUTSIDE
+	//      0          4.50           0.337        13.36  450.00 15.42
 }
