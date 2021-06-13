7800b6cfd77d94a582bc5ccc661755be1ed22e22
Konstantin8105 <Konstantin8105@gmail.com>
2021-06-13 15:51:58 +0300

    example 2 repor

--- thermal.go
+++ thermal.go
@@ -6,32 +6,190 @@ import (
 	"io"
 	"math"
 	"text/tabwriter"
+
+	"github.com/Konstantin8105/pow"
 )
 
+type Material interface {
+	// Conductivity(F float64) float64
+	ConductivityAvg(F1, F2 float64) float64
+}
+
+type MaterialPolynominal struct {
+	factors []float64
+}
+
+// func (m MaterialPolynominal) Conductivity(F float64) float64 {
+// 	K := make([]float64, len(m.factors))
+// 	for i := range m.factors {
+// 		K[i] = m.factors[i]
+// 	}
+// 	for i := 1; i < len(m.factors); i++ {
+// 		for j := i; j < len(m.factors); j++ {
+// 			K[j] *= F
+// 		}
+// 	}
+// 	Ksum := 0.0
+// 	for i := range K {
+// 		Ksum += K[i]
+// 	}
+// 	return Ksum
+// }
+
+func (m MaterialPolynominal) ConductivityAvg(F2, F1 float64) float64 {
+	K := make([]float64, len(m.factors))
+	for i := range m.factors {
+		K[i] = m.factors[i]
+	}
+	for i := 0; i < len(m.factors); i++ {
+		K[i] *= (pow.En(F2, i+1) - pow.En(F1, i+1)) / float64(i+1)
+	}
+	Ksum := 0.0
+	for i := range K {
+		Ksum += K[i]
+	}
+	Ksum = Ksum / (F2 - F1)
+	return Ksum
+}
+
+func NewMaterialPolynominal(c ...float64) Material {
+	return MaterialPolynominal{factors: c}
+}
+
+type MaterialExp struct {
+	a, b float64
+}
+
+func (m MaterialExp) ConductivityAvg(F1, F2 float64) float64 {
+	return 1.0 / (F2 - F1) * (math.Exp(m.a+m.b*F2) - math.Exp(m.a+m.b*F1)) / m.b
+}
+
+func NewMaterialExp(a, b float64) Material {
+	return MaterialExp{a: a, b: b}
+}
+
 type Layer struct {
-	Thk          float64
-	Conductivity func(F float64) float64
+	Thk float64
+	Mat Material
 }
 
-func (l Layer) Kavg(F1, F2 float64) float64 {
-	var (
-		amount = 10
-		K      float64
-		dF     = (F2 - F1) / float64(amount-1)
-	)
-	Ki := make([]float64, amount)
-	for i := 0; i < amount; i++ {
-		Ki[i] = l.Conductivity(F1 + float64(i)*dF)
-	}
-	for i := 0; i < amount-1; i++ {
-		K += (Ki[i] + Ki[i+1]) / 2.0 * dF
-	}
-	K = K / (F2 - F1)
-	return K
+// func integral(F1, F2 float64, c func(float64) float64) float64 {
+// 	var (
+// 		amount = 10000
+// 		K      float64
+// 		dF     = (F2 - F1) / float64(amount-1)
+// 	)
+// 	Ki := make([]float64, amount)
+// 	for i := 0; i < amount; i++ {
+// 		Ki[i] = c(F1 + float64(i)*dF)
+// 	}
+// 	for i := 0; i < amount-1; i++ {
+// 		K += (Ki[i] + Ki[i+1]) / 2.0 * dF
+// 	}
+// 	K = K / (F2 - F1)
+// 	return K
+// }
+
+type ExternalSurface struct {
+	isSurf bool
+	surf   float64
+	wind   float64
+	emiss  float64
+	orient Orientation
 }
 
-func Flat(o io.Writer, Tservice float64, layers []Layer, Tamb float64, Surf float64) (
+func Surf(Surf float64) *ExternalSurface {
+	es := new(ExternalSurface)
+	es.surf = Surf
+	es.isSurf = true
+	return es
+}
+
+func Emiss(Wind, Emiss float64, orient Orientation) *ExternalSurface {
+	es := new(ExternalSurface)
+	es.wind = Wind
+	es.emiss = Emiss
+	es.orient = orient
+	return es
+}
+
+func (es *ExternalSurface) surcof(Dia, Ts, Tamb float64, isCylinder bool) {
+	Tair := (Tamb+Ts)/2.0 + 459.69
+	ATdelt := math.Abs(Tamb - Ts)
+	if ATdelt < 1.0 {
+		ATdelt = 1.0
+	}
+
+	var Dx float64
+	var coef float64
+
+	if isCylinder {
+		Dx = Dia * 12.0
+		switch es.orient {
+		case 1:
+			coef = 1.016
+		case 2:
+			coef = 1.235
+		}
+		if 24 < Dx {
+			Dx = 24.0
+		}
+	} else {
+		Dx = 24.0
+		switch es.orient {
+		case 1:
+			coef = 1.394
+		case 2:
+			coef = 0.89
+		case 3:
+			coef = 1.79
+		}
+	}
+
+	var H, Hsamb, Hramb float64
+	Hsamb = coef * math.Pow(Dx, -0.2) * math.Pow(Tair, -0.181) * math.Pow(ATdelt, 0.266) * math.Sqrt(1+1.277*es.wind)
+	if Tamb != Ts {
+		Hramb = es.emiss * 0.1713e-8 * (pow.E4(Tamb+459.69) - pow.E4(Ts+459.69)) / (Tamb - Ts)
+	}
+	H = Hsamb + Hramb
+	if H < 0.0 {
+		H = 1.61
+	}
+
+	es.surf = H
+}
+
+// NOR = 1 vertical pipe
+// NOR = 2 horizontal PIPE
+
+// NOR = 1 horizontal heat FLOW
+// NOR = 2 heat flow down
+// NOR = 3 heat flow up
+
+type Orientation int8
+
+const (
+	FlatVerticalSurface Orientation = 1
+	FlatHeatFlowDown                = 2
+	FlatHeatFlowUp                  = 3
+	PipeVertical                    = 1
+	PipeHorizontal                  = 2
+)
+
+func Flat(o io.Writer, Tservice float64, layers []Layer, Tamb float64, es *ExternalSurface) (
 	Q float64, T []float64, err error) {
+	return calc(o, Tservice, layers, Tamb, es, -1.0)
+}
+
+func Cylinder(o io.Writer, Tservice float64, layers []Layer, Tamb float64, es *ExternalSurface, ODpipe float64) (
+	Q float64, T []float64, err error) {
+	return calc(o, Tservice, layers, Tamb, es, ODpipe)
+}
+
+func calc(o io.Writer, Tservice float64, layers []Layer, Tamb float64, es *ExternalSurface, ODpipe float64) (
+	Q float64, T []float64, err error) {
+
+	isCylinder := 0.0 < ODpipe
 
 	// nil output
 	if o == nil {
@@ -47,8 +205,27 @@ func Flat(o io.Writer, Tservice float64, layers []Layer, Tamb float64, Surf floa
 		// input data
 		fmt.Fprintf(out, "HEAT FLOW AND SURFACE TEMPERATURES OF INSULATED EQUPMENT PER ASTM C-680\n")
 		fmt.Fprintf(out, "\n")
-		fmt.Fprintf(out, "EQUPMENT SERVICE TEMPERATURE, F:\t %.1f\n", Tservice)
+		if isCylinder {
+			fmt.Fprintf(out, "PIPE OUSIDE INSULLATION:\t YES\n")
+			fmt.Fprintf(out, "ACTUAL PIPE DIAMETER, IN:\t %.1f\n", ODpipe)
+			fmt.Fprintf(out, "PIPE SERVICE TEMPERATURE, F:\t %.1f\n", Tservice)
+		} else {
+			fmt.Fprintf(out, "EQUPMENT SERVICE TEMPERATURE, F:\t %.1f\n", Tservice)
+		}
 		fmt.Fprintf(out, "AMBIENT TEMPERATURE, F:\t %.1f\n", Tamb)
+	}
+
+	// calculate diameters per layers
+	OD := make([]float64, len(layers))
+	ID := make([]float64, len(layers))
+	{
+		ID[0] = ODpipe
+		for i := range layers {
+			if 0 < i {
+				ID[i] = OD[i-1]
+			}
+			OD[i] = ID[i] + 2.0*layers[i].Thk
+		}
 	}
 
 	// temperature initialization
@@ -74,14 +251,17 @@ func Flat(o io.Writer, Tservice float64, layers []Layer, Tamb float64, Surf floa
 	for ; iter < iterMax; iter++ {
 		// symmary
 		var Rsum float64
-		if 0 < Surf {
-			Rsum = 1.0 / Surf
-		} else {
-			// Rsum = 1.0 / surcof(4, TS, Tamb, Emiss, Wind, Nor, 2.0)
+		if !es.isSurf {
+			es.surcof(OD[len(layers)-1], T[len(layers)], Tamb, isCylinder)
 		}
+		Rsum = 1.0 / es.surf
 		for i := range layers {
-			K[i] = layers[i].Kavg(T[i], T[i+1])
-			R[i] = layers[i].Thk / K[i]
+			K[i] = layers[i].Mat.ConductivityAvg(T[i], T[i+1])
+			if isCylinder {
+				R[i] = OD[len(layers)-1] / 2.0 * math.Log(OD[i]/ID[i]) / K[i]
+			} else {
+				R[i] = layers[i].Thk / K[i]
+			}
 			Rsum += R[i]
 		}
 
@@ -104,11 +284,18 @@ func Flat(o io.Writer, Tservice float64, layers []Layer, Tamb float64, Surf floa
 		return
 	}
 
+	if isCylinder {
+		Q = Q * math.Pi * OD[len(layers)-1] / 12.0
+	}
+
+	// TODO orientation view
 	{
 		// output data
-		if 0 < Surf {
-			fmt.Fprintf(out, "SURFACE COEF. USED, BTU/HR.SF.F:\t %.1f\n", Surf)
+		if !es.isSurf {
+			fmt.Fprintf(out, "EMITTANCE:\t %.1f\n", es.emiss)
+			fmt.Fprintf(out, "WIND SPEED, MPH:\t %.1f\n", es.wind)
 		}
+		fmt.Fprintf(out, "SURFACE COEF. USED, BTU/HR.SF.F:\t %.1f\n", es.surf)
 		fmt.Fprintf(out, "TOTAL HEAT FLUX, BTU/HR.SF:\t %.2f\n", Q)
 		fmt.Fprintf(out, "\n")
 		fmt.Fprintf(out, "LAYER \tINSULATION \tCONDUCTIVITY \tRESISTANCE \tTEMPERATURE,F\n")
--- thermal_test.go
+++ thermal_test.go
@@ -2,7 +2,6 @@ package thermal
 
 import (
 	"fmt"
-	"math"
 	"os"
 )
 
@@ -10,11 +9,9 @@ func ExampleProblem1() {
 	Q, T, err := Flat(os.Stdout, 450.0, []Layer{
 		Layer{
 			Thk: 4.0,
-			Conductivity: func(F float64) float64 {
-				return math.Exp(-1.62 + 0.213e-2*F)
-			},
+			Mat: NewMaterialExp(-1.62, 0.213e-2),
 		},
-	}, 10.0, 6.0)
+	}, 10.0, Surf(6.0))
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "%v", err)
 		return
@@ -28,22 +25,20 @@ func ExampleProblem1() {
 	//  EQUPMENT SERVICE TEMPERATURE, F: 450.0
 	//           AMBIENT TEMPERATURE, F: 10.0
 	//  SURFACE COEF. USED, BTU/HR.SF.F: 6.0
-	//       TOTAL HEAT FLUX, BTU/HR.SF: 36.57
+	//       TOTAL HEAT FLUX, BTU/HR.SF: 36.54
 	//
 	//  LAYER    INSULATION    CONDUCTIVITY   RESISTANCE TEMPERATURE,F
 	//     No  THICKNESS,in  BTU.IN/HR.SF.F  HR.SF.F/BTU  INSIDE OUTSIDE
-	//      0          4.00           0.337        11.87  450.00 16.09
+	//      0          4.00           0.337        11.88  450.00 16.09
 }
 
 func ExampleProblem1a() {
 	Q, T, err := Flat(os.Stdout, 450.0, []Layer{
 		Layer{
 			Thk: 4.5,
-			Conductivity: func(F float64) float64 {
-				return math.Exp(-1.62 + 0.213e-2*F)
-			},
+			Mat: NewMaterialExp(-1.62, 0.213e-2),
 		},
-	}, 10.0, 6.0)
+	}, 10.0, Surf(6.0))
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "%v", err)
 		return
@@ -57,9 +52,84 @@ func ExampleProblem1a() {
 	//  EQUPMENT SERVICE TEMPERATURE, F: 450.0
 	//           AMBIENT TEMPERATURE, F: 10.0
 	//  SURFACE COEF. USED, BTU/HR.SF.F: 6.0
-	//       TOTAL HEAT FLUX, BTU/HR.SF: 32.53
+	//       TOTAL HEAT FLUX, BTU/HR.SF: 32.51
 	//
 	//  LAYER    INSULATION    CONDUCTIVITY   RESISTANCE TEMPERATURE,F
 	//     No  THICKNESS,in  BTU.IN/HR.SF.F  HR.SF.F/BTU  INSIDE OUTSIDE
-	//      0          4.50           0.337        13.36  450.00 15.42
+	//      0          4.50           0.337        13.37  450.00 15.42
 }
+
+func ExampleProblem2() {
+	Q, T, err := Cylinder(os.Stdout, 800.0, []Layer{
+		Layer{
+			Thk: 2.0,
+			Mat: NewMaterialPolynominal(0.400, 0.105e-3, 0.286e-6),
+		},
+	}, 80.0, Surf(1.76), 3.5)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "%v", err)
+		return
+	}
+	_ = Q
+	_ = T
+
+	// Output:
+	// HEAT FLOW AND SURFACE TEMPERATURES OF INSULATED EQUPMENT PER ASTM C-680
+	//
+	//          PIPE OUSIDE INSULLATION: YES
+	//         ACTUAL PIPE DIAMETER, IN: 3.5
+	//      PIPE SERVICE TEMPERATURE, F: 800.0
+	//           AMBIENT TEMPERATURE, F: 80.0
+	//  SURFACE COEF. USED, BTU/HR.SF.F: 1.8
+	//       TOTAL HEAT FLUX, BTU/HR.SF: 234.80
+	//
+	//  LAYER    INSULATION    CONDUCTIVITY   RESISTANCE TEMPERATURE,F
+	//     No  THICKNESS,in  BTU.IN/HR.SF.F  HR.SF.F/BTU  INSIDE OUTSIDE
+	//      0          2.00           0.524         5.45  800.00 147.95
+}
+
+func ExampleProblem2a() {
+	Q, T, err := Cylinder(os.Stdout, 800.0, []Layer{
+		Layer{
+			Thk: 2.5,
+			Mat: NewMaterialPolynominal(0.400, 0.105e-3, 0.286e-6),
+		},
+	}, 80.0, Surf(1.76), 3.5)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "%v", err)
+		return
+	}
+	_ = Q
+	_ = T
+
+	// Output:
+	// HEAT FLOW AND SURFACE TEMPERATURES OF INSULATED EQUPMENT PER ASTM C-680
+	//
+	//          PIPE OUSIDE INSULLATION: YES
+	//         ACTUAL PIPE DIAMETER, IN: 3.5
+	//      PIPE SERVICE TEMPERATURE, F: 800.0
+	//           AMBIENT TEMPERATURE, F: 80.0
+	//  SURFACE COEF. USED, BTU/HR.SF.F: 1.8
+	//       TOTAL HEAT FLUX, BTU/HR.SF: 205.52
+	//
+	//  LAYER    INSULATION    CONDUCTIVITY   RESISTANCE TEMPERATURE,F
+	//     No  THICKNESS,in  BTU.IN/HR.SF.F  HR.SF.F/BTU  INSIDE OUTSIDE
+	//      0          2.50           0.522         7.23  800.00 132.47
+}
+
+// func ExampleProblem3() {
+// 	Q, T, err := Cylinder(os.Stdout, 800.0, []Layer{
+// 		Layer{
+// 			Thk: 3.0,
+// 			Mat: NewMaterialPolynominal(0.400, 0.105e-3, 0.286e-6),
+// 		},
+// 	}, 80.0, Emiss(0.0, 0.90, PipeHorizontal), 3.5)
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stdout, "%v", err)
+// 		return
+// 	}
+// 	_ = Q
+// 	_ = T
+// 
+// 	// Output:
+// }
